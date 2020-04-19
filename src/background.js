e74b84ee49fde0bf09fac7429f69b0fb9368bcd5
Christopher <chrdengso@gmail.com>
2020-04-19 17:17:31 +0800

    icons

--- src/background.js
+++ src/background.js
@@ -148,16 +148,12 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         showPartyConsole({ tabId });
       } else {
         chrome.storage.local.get(["party"], async (result) => {
-          console.log({ result });
-
           if (result?.party) {
             // start content script when page is loaded and we have a party
             let { data } = await client.query({
               query: PLAYLIST,
               variables: { id: result.party.playlist.id },
             });
-
-            console.log({ data });
 
             if (data) {
               showPartyConsole({ tabId });
@@ -177,4 +173,19 @@ chrome.tabs.onRemoved.addListener((tabId) => {
       subscriber.unsubscribe();
     }
   }
+});
+
+var rule2 = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: { hostEquals: "www.youtube.com", schemes: ["https"] },
+    }),
+  ],
+  actions: [new chrome.declarativeContent.ShowPageAction()],
+};
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([rule2]);
+  });
 });
