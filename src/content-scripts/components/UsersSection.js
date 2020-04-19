d42f9e02ee1990362024ba1d3bca5f0d79e617ee
Christopher <chrdengso@gmail.com>
2020-04-19 22:14:47 +0800

    save

--- src/content-scripts/components/UsersSection.js
+++ src/content-scripts/components/UsersSection.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const UsersSection = ({ users = [], style }) => {
+  const picUrl = chrome.runtime.getURL("images/empty_profile_picture.png");
+
   return (
-    <div className="section" style={style}>
+    <div className="section colored" style={style}>
       <ul className="users">
         {users.map((u) => (
-          <li key={u.id}>{u.name || u.id}</li>
+          <li key={u.id}>
+            <img src={picUrl} />
+            {u.name || u.id}
+          </li>
         ))}
       </ul>
     </div>
